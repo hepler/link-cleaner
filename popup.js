a53d88b9d34cd24f0d1e8b01dd02c8621daf4029
Kurt Hepler <kurt@codehs.com>
2015-07-22 13:50:21 -0700

    add images, use them for logo

--- popup.js
+++ popup.js
@@ -29,7 +29,7 @@ function cleanLink(link) {
         link = link.replace(/&.*$/,'');
         // now clean up the encoded URL pieces and remove junk at the end
         link = 'https://' + decodeURIComponent(link.replace(/\+/g, '%20'));
-        link = link.substring(0, link.indexOf('&t'));
+        // link = link.substring(0, link.indexOf('&t'));
     }
     // take off any UTM codes at the end
     if(link.indexOf('?utm') > 0) {
